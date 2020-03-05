43b3f1cb17f3a11ca0f49539b36bae0557ce631f
Corbin Crutchley <corbin.crutchley.contractor@hilton.com>
2020-03-05 14:11:44 -0800

    Rename from GitGUI to GitShark

--- android/app/src/main/java/com/gitshark/MainActivity.java
+++ android/app/src/main/java/com/gitshark/MainActivity.java
@@ -1,4 +1,4 @@
-package com.gitgui;
+package com.gitshark;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "GitGui";
+    return "GitShark";
   }
 }
--- android/app/src/main/java/com/gitshark/MainApplication.java
+++ android/app/src/main/java/com/gitshark/MainApplication.java
@@ -1,4 +1,4 @@
-package com.gitgui;
+package com.gitshark;
 
 import android.app.Application;
 import android.content.Context;
