089ef54a0ff76ebed930b7dc91a2b77a7d36603d
Corbin Crutchley <corbin.crutchley.contractor@hilton.com>
2020-03-06 06:38:49 -0800

    Added branch selection styling to history view

---
+++ src/components/history-branch-dropdown/history-branch-dropdown.tsx
@@ -0,0 +1,77 @@
+import {StyleSheet, Text, View} from 'react-native';
+import * as React from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {theme} from '../../constants/theme';
+import {TouchableRipple} from 'react-native-paper';
+import {textStyles} from '../../constants/text-styles';
+
+interface HistoryBranchDropdownProps {
+  branchName: string;
+  onFavorite: () => void;
+  favorite: boolean;
+}
+export const HistoryBranchDropdown = ({
+  branchName,
+  favorite,
+  onFavorite,
+}: HistoryBranchDropdownProps) => {
+  return (
+    <TouchableRipple
+      style={styles.dropdownContinaer}
+      onPress={() => {}}
+      rippleColor={theme.colors.outlineColor}>
+      <View style={styles.dropdownView}>
+        <Text numberOfLines={1} style={styles.branchName}>
+          {branchName}
+        </Text>
+        <TouchableRipple
+          style={styles.starButton}
+          onPress={onFavorite}
+          rippleColor={theme.colors.outlineColor}>
+          <Icon
+            name={favorite ? 'star' : 'star-outline'}
+            size={24}
+            color={theme.colors.accent}
+          />
+        </TouchableRipple>
+        <View style={styles.buttonDivider} />
+        <Icon
+          name="chevron-down"
+          size={24}
+          color={theme.colors.accent}
+          style={styles.dropdownButton}
+        />
+      </View>
+    </TouchableRipple>
+  );
+};
+
+const styles = StyleSheet.create({
+  dropdownContinaer: {
+    paddingLeft: 16,
+  },
+  dropdownView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  branchName: {
+    marginRight: 16,
+    flexGrow: 1,
+    ...textStyles.callout,
+    fontWeight: 'bold',
+  },
+  starButton: {
+    borderRadius: 50,
+    padding: 8,
+    marginRight: 7,
+  },
+  buttonDivider: {
+    height: 36,
+    width: 1,
+    backgroundColor: theme.colors.outlineColor,
+  },
+  dropdownButton: {
+    paddingHorizontal: 16,
+    paddingVertical: 24,
+  },
+});
--- src/views/repository-history/repository-history.tsx
+++ src/views/repository-history/repository-history.tsx
@@ -3,6 +3,7 @@ import {StyleSheet, View, ScrollView} from 'react-native';
 
 import {RepoContext} from '../../constants/repo-context';
 import {CommitList} from '../../components/commit-list/commit-list';
+import {HistoryBranchDropdown} from '../../components/history-branch-dropdown/history-branch-dropdown';
 
 export const RepositoryHistory = () => {
   const {repo} = React.useContext(RepoContext);
@@ -10,6 +11,11 @@ export const RepositoryHistory = () => {
   return (
     <>
       <View style={styles.container}>
+        <HistoryBranchDropdown
+          onFavorite={() => {}}
+          favorite={false}
+          branchName={'the_big_branch'}
+        />
         <ScrollView>
           <CommitList />
         </ScrollView>
