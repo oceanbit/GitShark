ff18cb4bb2c1c780720320553ddf53a502113ada
Corbin Crutchley <crutchcorn@gmail.com>
2020-03-31 20:36:20 -0700

    Got mocked "staging" and "unstaging" working

--- src/views/repository-changes/repository-changes.tsx
+++ src/views/repository-changes/repository-changes.tsx
@@ -15,7 +15,12 @@ import {theme} from '../../constants/theme';
 
 export const RepositoryChanges = () => {
   const {repo} = React.useContext(RepoContext);
-  const [changes, setChanges] = React.useState<ChangesArrayItem[]>([]);
+  const [stagedChanges, setStagedChanges] = React.useState<ChangesArrayItem[]>(
+    [],
+  );
+  const [unstagedChanges, setUnstagedChanges] = React.useState<
+    ChangesArrayItem[]
+  >([]);
   const [showUnstagedDivider, setShowUnstagedDivider] = React.useState(false);
   const [showStagedDivider, setShowStagedDivider] = React.useState(false);
 
@@ -27,13 +32,43 @@ export const RepositoryChanges = () => {
       const onlyChangedFiles = newFiles.filter(
         file => file.fileStatus !== 'unmodified',
       );
-      setChanges(onlyChangedFiles);
+      const [unstaged, staged] = onlyChangedFiles.reduce(
+        (prev, change) => {
+          if (change.staged) {
+            prev[1].push(change);
+          } else {
+            prev[0].push(change);
+          }
+          return prev;
+        },
+        [[], []],
+      );
+      setStagedChanges(unstaged);
+      setUnstagedChanges(staged);
     });
   }, [repo]);
 
   React.useEffect(() => {
     getUpdate();
   }, [getUpdate]);
+
+  const addToStaged = (change: ChangesArrayItem) => {
+    const newUnstaged = unstagedChanges.filter(
+      unChange => unChange.fileName !== change.fileName,
+    );
+    const newStaged = [...stagedChanges, change];
+    setUnstagedChanges(newUnstaged);
+    setStagedChanges(newStaged);
+  };
+
+  const removeFromStaged = (change: ChangesArrayItem) => {
+    const newStaged = stagedChanges.filter(
+      unChange => unChange.fileName !== change.fileName,
+    );
+    const newUnstaged = [...unstagedChanges, change];
+    setUnstagedChanges(newUnstaged);
+    setStagedChanges(newStaged);
+  };
 
   const onUnstagedScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
     if (!event.nativeEvent.contentOffset.y) {
@@ -54,7 +89,7 @@ export const RepositoryChanges = () => {
   return (
     <>
       <View style={styles.container}>
-        <View style={styles.halfSection}>
+        <View style={[styles.halfSection, styles.firstSection]}>
           <SubheaderWithButton
             buttonText={'Stage All'}
             calloutText={'Unstaged'}
@@ -62,20 +97,12 @@ export const RepositoryChanges = () => {
             style={showUnstagedDivider ? styles.underlineHeader : {}}
           />
           <ScrollView style={styles.changesList} onScroll={onUnstagedScroll}>
-            {changes.map(props => {
+            {unstagedChanges.map(props => {
               return (
                 <FileChangeListItemWithCheckbox
                   isChecked={false}
                   key={props.fileName}
-                  {...props}
-                />
-              );
-            })}
-            {changes.map(props => {
-              return (
-                <FileChangeListItemWithCheckbox
-                  isChecked={false}
-                  key={props.fileName}
+                  onToggle={() => addToStaged(props)}
                   {...props}
                 />
               );
@@ -90,11 +117,12 @@ export const RepositoryChanges = () => {
             style={showStagedDivider ? styles.underlineHeader : {}}
           />
           <ScrollView style={styles.changesList} onScroll={onStagedScroll}>
-            {changes.map(props => {
+            {stagedChanges.map(props => {
               return (
                 <FileChangeListItemWithCheckbox
-                  isChecked={false}
+                  isChecked={true}
                   key={props.fileName}
+                  onToggle={() => removeFromStaged(props)}
                   {...props}
                 />
               );
@@ -126,6 +154,10 @@ const styles = StyleSheet.create({
   },
   halfSection: {
     height: '50%',
+  },
+  firstSection: {
+    borderBottomColor: theme.colors.outlineColor,
+    borderBottomWidth: 1,
   },
   underlineHeader: {
     borderBottomColor: theme.colors.outlineColor,
