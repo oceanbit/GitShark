5232787b3a460127bd5057ecd7f557bdc8449f23
Corbin Crutchley <crutchcorn@gmail.com>
2020-03-31 19:29:23 -0700

    Added Shark Button component to match mocks

---
+++ src/components/shark-button/shark-button.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import {
+  StyleSheet,
+  View,
+  StyleProp,
+  ViewStyle,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {theme} from '../../constants/theme';
+import {textStyles} from '../../constants/text-styles';
+import { TouchableRipple } from 'react-native-paper';
+
+interface SharkTextInputProps {
+  onPress: () => void;
+  style?: StyleProp<ViewStyle>;
+  text: string;
+  icon?: string;
+  type?: 'outline' | 'primary';
+}
+export const SharkButton = ({
+  onPress,
+  text,
+  icon,
+  style = {},
+  type = 'outline',
+}: SharkTextInputProps) => {
+  const buttonPaddingStyle = icon ? styles.iconButton : styles.noIconButton;
+  const buttonTypeStyle =
+    type === 'outline' ? styles.outlineButton : styles.primaryButton;
+  const iconColor = type === 'outline' ? theme.colors.accent : 'white';
+  return (
+    <TouchableRipple
+      onPress={onPress}
+      style={[styles.button, buttonPaddingStyle, buttonTypeStyle, style]}>
+      {!!icon && (
+        <View style={styles.iconView}>
+          <Icon size={24} name={icon} color={iconColor} />
+        </View>
+      )}
+      {text}
+    </TouchableRipple>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: theme.roundness,
+    alignContent: 'center',
+    ...textStyles.callout,
+  },
+  noIconButton: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  iconButton: {
+    paddingLeft: 12,
+    paddingRight: 16,
+    paddingVertical: 8,
+  },
+  outlineButton: {
+    borderColor: theme.colors.outlineColor,
+    borderWidth: 2,
+    backgroundColor: 'white',
+    color: theme.colors.accent,
+  },
+  primaryButton: {
+    backgroundColor: theme.colors.accent,
+    color: 'white',
+  },
+  iconView: {
+    marginRight: 8,
+  },
+});
