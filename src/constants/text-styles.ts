784c8bea932f073114c34590e7eff03393634577
Corbin Crutchley <crutchcorn@gmail.com>
2020-04-02 15:17:57 -0700

    Fix font display showing Rubik now, fix loading spinner in clone

--- src/constants/text-styles.ts
+++ src/constants/text-styles.ts
@@ -1,63 +1,63 @@
 export const textStyles = {
   display: {
-    fontFamily: 'Rubik',
-    fontWeight: 'bold' as 'bold',
+    fontFamily: 'rubik_bold',
+    fontWeight: 'normal' as 'normal',
     fontSize: 48,
     lineHeight: 64,
   },
 
   headline_01: {
-    fontFamily: 'Rubik',
-    fontWeight: 'bold' as 'bold',
+    fontFamily: 'rubik_bold',
+    fontWeight: 'normal' as 'normal',
     fontSize: 34,
     lineHeight: 48,
   },
 
   headline_02: {
-    fontFamily: 'Rubik',
-    fontWeight: 'bold' as 'bold',
+    fontFamily: 'rubik_bold',
+    fontWeight: 'normal' as 'normal',
     fontSize: 28,
     lineHeight: 36,
   },
 
   headline_03: {
-    fontFamily: 'Rubik',
-    fontWeight: '500' as '500',
+    fontFamily: 'rubik_medium',
+    fontWeight: 'normal' as 'normal',
     fontSize: 20,
     lineHeight: 28,
   },
 
   callout: {
-    fontFamily: 'Rubik',
-    fontWeight: '700' as '700',
+    fontFamily: 'rubik_medium',
+    fontWeight: 'normal' as 'normal',
     fontSize: 16,
     lineHeight: 24,
   },
 
   body_01: {
-    fontFamily: 'Rubik',
-    fontWeight: '400' as '400',
+    fontFamily: 'rubik_regular',
+    fontWeight: 'normal' as 'normal',
     fontSize: 16,
     lineHeight: 24,
   },
 
   body_02: {
-    fontFamily: 'Rubik',
-    fontWeight: '400' as '400',
+    fontFamily: 'rubik_regular',
+    fontWeight: 'normal' as 'normal',
     fontSize: 14,
     lineHeight: 20,
   },
 
   caption_01: {
-    fontFamily: 'Rubik',
-    fontWeight: '700' as '700',
+    fontFamily: 'rubik_medium',
+    fontWeight: 'normal' as 'normal',
     fontSize: 12,
     lineHeight: 16,
   },
 
   caption_02: {
-    fontFamily: 'Rubik',
-    fontWeight: '400' as '400',
+    fontFamily: 'rubik_regular',
+    fontWeight: 'normal' as 'normal',
     fontSize: 12,
     lineHeight: 16,
   },
