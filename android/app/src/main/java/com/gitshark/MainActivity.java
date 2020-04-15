4d2236961684aeae2ccd8d407b9021b6f631f980
Corbin Crutchley <crutchcorn@gmail.com>
2020-04-14 19:08:36 -0700

    Got transparent navbar working
    Thank you so so much @msasikanth

--- android/app/src/main/java/com/gitshark/MainActivity.java
+++ android/app/src/main/java/com/gitshark/MainActivity.java
@@ -11,25 +11,23 @@ import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "GitShark";
-  }
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "GitShark";
+    }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-      Window w = getWindow();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Window w = getWindow();
 
-      // FLAG_LAYOUT_NO_LIMITS - Window flag: allow window to extend outside of the screen.
-      // w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-w.setStatusBarColor(Color.TRANSPARENT);
-w.setNavigationBarColor(Color.TRANSPARENT);
-w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        w.setStatusBarColor(Color.TRANSPARENT);
+        w.setNavigationBarColor(Color.TRANSPARENT);
+        w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
 
-      super.onCreate(savedInstanceState);
-  }
+        super.onCreate(savedInstanceState);
+    }
 }
