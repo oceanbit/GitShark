30b96f436547b58c0ac9e937c6bebe82631c9813
Corbin Crutchley <crutchcorn@gmail.com>
2020-04-20 19:34:03 -0700

    Made sure that keyboard doesn't cover text inputs

--- src/views/account/account.tsx
+++ src/views/account/account.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
-import {ScrollView, Linking, Text, View} from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Linking,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
 import {AppBar} from '../../components/app-bar';
 import {SharkSubheader} from '../../components/shark-subheader';
 import {textStyles, theme, UserContext} from '../../constants';
@@ -72,86 +78,91 @@ export const Account = () => {
   };
 
   return (
-    <ScrollView style={styles.container}>
-      <TopSpacerView />
-      <AppBar
-        leftIcon="arrow-left"
-        onLeftSelect={() => history.goBack()}
-        headline="Accounts"
-      />
-      <SharkSubheader calloutText="GitHub integration" />
-      <SharkButton
-        style={styles.signinGithubButton}
-        text="Sign in with GitHub"
-        type="primary"
-        icon={'github-circle'}
-        onPress={() => {
-          Linking.openURL(githubOauthLink);
-        }}
-      />
-      <SharkSubheader
-        style={styles.commitAuthoringHeader}
-        calloutText="Commit authoring"
-      />
-      <View style={styles.commitAuthorContainer}>
-        <View style={styles.authorPreview}>
-          <SharkProfilePic source={authorImage} showGHLogo={isGitHub} />
-          <View style={styles.authorPreviewText}>
-            <Text style={styles.authorName}>{personName}</Text>
-            <Text style={styles.authorEmail}>{personEmail}</Text>
-          </View>
-        </View>
-        <TouchableRipple
-          style={[
-            styles.useGHCredsContainer,
-            !gitHubUser ? styles.disabledStyling : {},
-          ]}
-          onPress={() => {
-            setUseGithub(!useGitHub);
-            setManualName('');
-            setManualEmail('');
-          }}
-          disabled={!gitHubUser}>
-          <>
-            <View style={styles.checkboxContainer}>
-              <SharkCheckbox checked={useGitHub} onValueChange={() => {}} />
-            </View>
-            <Text style={styles.useGHText}>Use GitHub credentials</Text>
-          </>
-        </TouchableRipple>
-        <SharkTextInput
-          style={styles.textInput}
-          placeholder={personName}
-          value={manualName}
-          disabled={useGitHub}
-          errorStr={manualNameError}
-          onChangeText={val => {
-            setManualName(val);
-            setManualNameError('');
-          }}
+    <KeyboardAvoidingView
+      style={{flex: 1, flexDirection: 'column'}}
+      behavior="height"
+      enabled>
+      <ScrollView style={styles.container}>
+        <TopSpacerView />
+        <AppBar
+          leftIcon="arrow-left"
+          onLeftSelect={() => history.goBack()}
+          headline="Accounts"
         />
-        <SharkTextInput
-          style={styles.textInput}
-          placeholder={personEmail}
-          value={manualEmail}
-          disabled={useGitHub}
-          errorStr={manualEmailError}
-          onChangeText={val => {
-            setManualEmail(val);
-            setManualEmailError('');
-          }}
-          keyboardType={'email-address'}
-        />
+        <SharkSubheader calloutText="GitHub integration" />
         <SharkButton
-          style={styles.saveButton}
-          text="Save changes"
-          onPress={() => saveChanges()}
+          style={styles.signinGithubButton}
+          text="Sign in with GitHub"
           type="primary"
-          disabled={useGitHub}
+          icon={'github-circle'}
+          onPress={() => {
+            Linking.openURL(githubOauthLink);
+          }}
         />
-      </View>
-      <BottomSpacerView />
-    </ScrollView>
+        <SharkSubheader
+          style={styles.commitAuthoringHeader}
+          calloutText="Commit authoring"
+        />
+        <View style={styles.commitAuthorContainer}>
+          <View style={styles.authorPreview}>
+            <SharkProfilePic source={authorImage} showGHLogo={isGitHub} />
+            <View style={styles.authorPreviewText}>
+              <Text style={styles.authorName}>{personName}</Text>
+              <Text style={styles.authorEmail}>{personEmail}</Text>
+            </View>
+          </View>
+          <TouchableRipple
+            style={[
+              styles.useGHCredsContainer,
+              !gitHubUser ? styles.disabledStyling : {},
+            ]}
+            onPress={() => {
+              setUseGithub(!useGitHub);
+              setManualName('');
+              setManualEmail('');
+            }}
+            disabled={!gitHubUser}>
+            <>
+              <View style={styles.checkboxContainer}>
+                <SharkCheckbox checked={useGitHub} onValueChange={() => {}} />
+              </View>
+              <Text style={styles.useGHText}>Use GitHub credentials</Text>
+            </>
+          </TouchableRipple>
+          <SharkTextInput
+            style={styles.textInput}
+            placeholder={personName}
+            value={manualName}
+            disabled={useGitHub}
+            errorStr={manualNameError}
+            onChangeText={val => {
+              setManualName(val);
+              setManualNameError('');
+            }}
+          />
+          <SharkTextInput
+            style={styles.textInput}
+            placeholder={personEmail}
+            value={manualEmail}
+            disabled={useGitHub}
+            errorStr={manualEmailError}
+            onChangeText={val => {
+              setManualEmail(val);
+              setManualEmailError('');
+            }}
+            keyboardType={'email-address'}
+          />
+          <SharkButton
+            style={styles.saveButton}
+            text="Save changes"
+            onPress={() => saveChanges()}
+            type="primary"
+            disabled={useGitHub}
+          />
+        </View>
+        <BottomSpacerView />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
