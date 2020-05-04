e22a2bd7887c5c4dae8f1ad84f8719752458eaf6
Corbin Crutchley <crutchcorn@gmail.com>
2020-05-03 17:12:28 -0700

    Improved dropdown styling a bit more

--- src/views/branches/branches.tsx
+++ src/views/branches/branches.tsx
@@ -3,7 +3,7 @@
  * "repository-history" as it's part of the dropdown component there
  */
 import * as React from 'react';
-import {ScrollView, View} from 'react-native';
+import {ScrollView, View, Text} from 'react-native';
 import {DynamicStyleSheet, useDynamicStyleSheet} from 'react-native-dark-mode';
 import {SharkSubheader} from '../../components/shark-subheader';
 import {SharkDivider} from '../../components/shark-divider';
@@ -11,6 +11,9 @@ import {BranchListItem} from '../../components/branch-list-item';
 import {RemoteBranchListItem} from '../../components/remote-branch-list-item';
 import {DropdownContent} from '../../components/dropdown-content';
 import {AnimatedDropdownArrow} from '../../components/animated-dropdown-arrow';
+import {SharkIconButton} from '../../components/shark-icon-button';
+import {textStyles, theme} from '../../constants';
+import {TouchableRipple} from 'react-native-paper';
 
 export const Branches = () => {
   const styles = useDynamicStyleSheet(dynamicStyles);
@@ -38,76 +41,34 @@ export const Branches = () => {
           down: 2,
         }}
       />
-      <SharkDivider />
+      <SharkDivider style={styles.remoteDivider} />
       <SharkSubheader calloutText="Remotes" buttonText="Add new" />
-
-      <View style={{display: 'flex', width: 40}}>
-        <AnimatedDropdownArrow setExpanded={setExpanded} expanded={expanded} />
-      </View>
-
-      <DropdownContent expanded={expanded}>
+      <TouchableRipple
+        style={styles.dropDownHeader}
+        onPress={() => setExpanded(v => !v)}>
         <>
-          <RemoteBranchListItem
-            branch={{
-              name: 'develop',
-            }}
-            style={styles.remoteBranch}
+          <AnimatedDropdownArrow
+            setExpanded={setExpanded}
+            expanded={expanded}
           />
-          <RemoteBranchListItem
-            branch={{
-              name: 'master',
-            }}
-            style={styles.remoteBranch}
-          />
-          <RemoteBranchListItem
-            branch={{
-              name: 'develop',
-            }}
-            style={styles.remoteBranch}
-          />
-          <RemoteBranchListItem
-            branch={{
-              name: 'master',
-            }}
-            style={styles.remoteBranch}
-          />
-          <RemoteBranchListItem
-            branch={{
-              name: 'develop',
-            }}
-            style={styles.remoteBranch}
-          />
-          <RemoteBranchListItem
-            branch={{
-              name: 'master',
-            }}
-            style={styles.remoteBranch}
-          />
-          <RemoteBranchListItem
-            branch={{
-              name: 'develop',
-            }}
-            style={styles.remoteBranch}
-          />
-          <RemoteBranchListItem
-            branch={{
-              name: 'master',
-            }}
-            style={styles.remoteBranch}
-          />
-          <RemoteBranchListItem
-            branch={{
-              name: 'develop',
-            }}
-            style={styles.remoteBranch}
-          />
-          <RemoteBranchListItem
-            branch={{
-              name: 'master',
-            }}
-            style={styles.remoteBranch}
-          />
+          {/* This is a mock, we'll need to replace it with the list of real remotes soon */}
+          <Text style={styles.remoteHeader}>origin</Text>
+          <SharkIconButton iconName="dots-horizontal" onPress={() => {}} />
         </>
+      </TouchableRipple>
+      <DropdownContent expanded={expanded}>
+        <RemoteBranchListItem
+          branch={{
+            name: 'develop',
+          }}
+          style={styles.remoteBranch}
+        />
+        <RemoteBranchListItem
+          branch={{
+            name: 'master',
+          }}
+          style={styles.remoteBranch}
+        />
       </DropdownContent>
     </ScrollView>
   );
@@ -119,5 +80,20 @@ const dynamicStyles = new DynamicStyleSheet({
   },
   remoteBranch: {
     marginLeft: 40,
+  },
+  remoteDivider: {
+    marginTop: 16,
+  },
+  dropDownHeader: {
+    padding: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  remoteHeader: {
+    marginHorizontal: 8,
+    color: theme.colors.on_surface,
+    width: 1,
+    flexGrow: 1,
+    ...textStyles.callout,
   },
 });
