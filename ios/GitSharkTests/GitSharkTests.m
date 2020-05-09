0128a79c3b88c696b401d08438aef7390db4408d
Corbin Crutchley <corbin.crutchley@websystem3.com>
2020-05-08 21:24:49 -0700

    Added more fixes for iOS

--- ios/GitSharkTests/GitSharkTests.m
+++ ios/GitSharkTests/GitSharkTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface GitGuiTests : XCTestCase
+@interface GitSharkTests : XCTestCase
 
 @end
 
-@implementation GitGuiTests
+@implementation GitSharkTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
