1216d82eb5a8e65c70e06cc4691f22206e35ebac
Corbin Crutchley <crutchcorn@gmail.com>
2020-05-14 18:08:38 -0700

    Added commit details header ui

--- src/views/commit-details/commit-details.tsx
+++ src/views/commit-details/commit-details.tsx
@@ -1,19 +1,29 @@
 import * as React from 'react';
-import {View} from 'react-native';
-import {DynamicStyleSheet, useDynamicStyleSheet} from 'react-native-dark-mode';
+import {View, Text} from 'react-native';
+import {
+  DynamicStyleSheet,
+  useDynamicStyleSheet,
+  useDynamicValue,
+} from 'react-native-dark-mode';
 import {CommitDetailsHeader} from '../../components/commit-details-header';
 import {AppBar} from '../../components/app-bar';
 import {SharkMenu} from '../../components/shark-menu';
 import {Menu} from 'react-native-paper';
 import {SharkIconButton} from '../../components/shark-icon-button';
 import {SharkDivider} from '../../components/shark-divider';
+import {textStyles, theme} from '../../constants';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export const CommitDetails = () => {
+  const styles = useDynamicStyleSheet(dynamicStyles);
+  const change_addition = useDynamicValue(theme.colors.change_addition);
+  const change_removal = useDynamicValue(theme.colors.change_removal);
+  const change_mixed = useDynamicValue(theme.colors.change_mixed);
+
   const [headerExpanded, setHeaderExpanded] = React.useState(false);
   const [headerMessageExpanded, setHeaderMessageExpanded] = React.useState(
     false,
   );
-  const styles = useDynamicStyleSheet(dynamicStyles);
 
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
@@ -47,6 +57,28 @@ export const CommitDetails = () => {
           setMessageExpanded={setHeaderMessageExpanded}
         />
         <SharkDivider />
+        <View style={styles.changesHeader}>
+          <Text style={styles.changesHeaderText}>Changes</Text>
+          <View style={styles.growContainer} />
+          <View style={styles.infoBlock}>
+            <Icon name="plus-circle" size={24} color={change_addition} />
+            <Text style={[styles.iconText, styles.additionText]}>1</Text>
+          </View>
+
+          <View style={[styles.infoBlock]}>
+            <Icon name="minus-circle" size={24} color={change_removal} />
+            <Text style={[styles.iconText, styles.removalText]}>1</Text>
+          </View>
+
+          <View style={[styles.infoBlock]}>
+            <Icon
+              name="dots-horizontal-circle"
+              size={24}
+              color={change_mixed}
+            />
+            <Text style={[styles.iconText, styles.modifiedText]}>1</Text>
+          </View>
+        </View>
       </View>
     </>
   );
@@ -54,4 +86,37 @@ export const CommitDetails = () => {
 
 const dynamicStyles = new DynamicStyleSheet({
   container: {},
+  changesHeader: {
+    padding: 16,
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'center',
+  },
+  growContainer: {
+    flexGrow: 1,
+  },
+  changesHeaderText: {
+    ...textStyles.callout,
+    color: theme.colors.on_surface,
+  },
+  infoBlock: {
+    marginLeft: 8,
+    paddingHorizontal: 4,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconText: {
+    marginLeft: 4,
+    ...textStyles.caption_01,
+  },
+  additionText: {
+    color: theme.colors.change_addition,
+  },
+  removalText: {
+    color: theme.colors.change_removal,
+  },
+  modifiedText: {
+    color: theme.colors.change_mixed,
+  },
+  movedText: {},
 });
