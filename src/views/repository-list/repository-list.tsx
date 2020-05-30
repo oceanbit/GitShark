3912a8a0352366f0c9e84f9ed791eaaddb90a18e
Corbin Crutchley <crutchcorn@gmail.com>
2020-05-30 15:37:15 -0700

    Initial work on repo list story

--- src/views/repository-list/repository-list.tsx
+++ src/views/repository-list/repository-list.tsx
@@ -1,20 +1,13 @@
 import * as React from 'react';
-import {Alert, ScrollView, Text, View} from 'react-native';
+import {Alert} from 'react-native';
 import {Repo} from '@entities';
 import {getRepository} from 'typeorm';
-import {RepoCard} from './components/repo-card';
-import {textStyles, theme} from '@constants';
-import {RepoListLoading} from './components/repo-list-loading';
-import {DialogsAndFab} from './components/dialogs-and-fab';
-import {SharkIconButton} from '@components/shark-icon-button';
 import {useNavigation} from '@react-navigation/native';
-import {DynamicStyleSheet, useDynamicStyleSheet} from 'react-native-dark-mode';
-import {BottomSpacerView, SharkSafeTop} from '../../components/shark-safe-top';
 import {useSelector} from 'react-redux';
 import {RootState} from '@store';
+import {RepositoryListUI} from './repository-list.ui';
 
 export const RepositoryList = () => {
-  const styles = useDynamicStyleSheet(dynamicStyles);
   const {isLoaded: isDBLoaded} = useSelector(
     (state: RootState) => state.database,
   );
@@ -43,78 +36,17 @@ export const RepositoryList = () => {
     findRepos();
   }, [findRepos, isDBLoaded]);
 
+  const navigateToSettings = () => {
+    history.navigate('Settings');
+  };
+
   return (
-    <SharkSafeTop>
-      <View style={styles.container}>
-        <View style={styles.headingContainer}>
-          <Text style={styles.headingText}>Repositories</Text>
-          <SharkIconButton
-            onPress={() => {
-              history.navigate('Settings');
-            }}
-            iconName={'settings-outline'}
-          />
-        </View>
-        {isLoading && <RepoListLoading />}
-        {!isLoading && !!repos?.length && (
-          <ScrollView>
-            {repos!.map(repo => {
-              return (
-                <RepoCard
-                  key={repo.id}
-                  repo={repo}
-                  onUpdate={() => findRepos()}
-                />
-              );
-            })}
-            <BottomSpacerView additionalSpacing={30} />
-          </ScrollView>
-        )}
-      </View>
-      <DialogsAndFab
-        isDBLoaded={isDBLoaded}
-        isLoading={isLoading}
-        repos={repos}
-        findRepos={findRepos}
-      />
-    </SharkSafeTop>
+    <RepositoryListUI
+      isLoading={isLoading}
+      isDBLoaded={isDBLoaded}
+      navigateToSettings={navigateToSettings}
+      repos={repos}
+      findRepos={findRepos}
+    />
   );
 };
-
-const dynamicStyles = new DynamicStyleSheet({
-  container: {
-    paddingTop: 16,
-    paddingHorizontal: 16,
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  headingContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'nowrap',
-    alignItems: 'center',
-    marginBottom: 16,
-  },
-  headingText: {
-    flexGrow: 1,
-    ...textStyles.headline_01,
-    color: theme.colors.on_surface,
-  },
-  noRepos: {
-    ...textStyles.headline_01,
-    color: theme.colors.on_surface,
-    opacity: 0.4,
-    position: 'absolute',
-    textAlign: 'center',
-    width: '100%',
-    // Edgecase for parent padding
-    left: 16,
-  },
-  fabview: {
-    position: 'absolute',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    width: '100%',
-  },
-});
---
+++ src/views/repository-list/repository-list.ui.tsx
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import {Alert, ScrollView, Text, View} from 'react-native';
+import {Repo} from '@entities';
+import {RepoCard} from './components/repo-card';
+import {textStyles, theme} from '@constants';
+import {RepoListLoading} from './components/repo-list-loading';
+import {DialogsAndFab} from './components/dialogs-and-fab';
+import {SharkIconButton} from '@components/shark-icon-button';
+import {DynamicStyleSheet, useDynamicStyleSheet} from 'react-native-dark-mode';
+import {BottomSpacerView, SharkSafeTop} from '../../components/shark-safe-top';
+
+interface RepositoryListUIProps {
+  isLoading: boolean;
+  isDBLoaded: boolean;
+  navigateToSettings: () => void;
+  repos: Repo[] | null;
+  findRepos: () => Promise<boolean | undefined>;
+}
+export const RepositoryListUI = ({
+  isLoading,
+  isDBLoaded,
+  navigateToSettings,
+  repos,
+  findRepos,
+}: RepositoryListUIProps) => {
+  const styles = useDynamicStyleSheet(dynamicStyles);
+
+  return (
+    <SharkSafeTop>
+      <View style={styles.container}>
+        <View style={styles.headingContainer}>
+          <Text style={styles.headingText}>Repositories</Text>
+          <SharkIconButton
+            onPress={navigateToSettings}
+            iconName={'settings-outline'}
+          />
+        </View>
+        {isLoading && <RepoListLoading />}
+        {!isLoading && !!repos?.length && (
+          <ScrollView>
+            {repos!.map(repo => {
+              return (
+                <RepoCard
+                  key={repo.id}
+                  repo={repo}
+                  onUpdate={() => findRepos()}
+                />
+              );
+            })}
+            <BottomSpacerView additionalSpacing={30} />
+          </ScrollView>
+        )}
+      </View>
+      <DialogsAndFab
+        isDBLoaded={isDBLoaded}
+        isLoading={isLoading}
+        repos={repos}
+        findRepos={findRepos}
+      />
+    </SharkSafeTop>
+  );
+};
+
+const dynamicStyles = new DynamicStyleSheet({
+  container: {
+    paddingTop: 16,
+    paddingHorizontal: 16,
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  headingContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  headingText: {
+    flexGrow: 1,
+    ...textStyles.headline_01,
+    color: theme.colors.on_surface,
+  },
+  noRepos: {
+    ...textStyles.headline_01,
+    color: theme.colors.on_surface,
+    opacity: 0.4,
+    position: 'absolute',
+    textAlign: 'center',
+    width: '100%',
+    // Edgecase for parent padding
+    left: 16,
+  },
+  fabview: {
+    position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+  },
+});
