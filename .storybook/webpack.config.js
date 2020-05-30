e64c404cc33fd197b9570a0901286901b526193e
Corbin Crutchley <crutchcorn@gmail.com>
2020-05-30 14:41:35 -0700

    Fix storybook icon loading

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 const fs = require('fs');
 const appDirectory = fs.realpathSync(process.cwd());
 
@@ -6,49 +6,47 @@ const webpack = require('webpack');
 
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
-module.exports = ({ config, mode }) => {
-  config.module.rules.push(
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: [/node_modules/],
-        loader: require.resolve('babel-loader'),
-        options: {
-          babelrc: false,
-          presets: [['react-app', { flow: false, typescript: true }]],
-          plugins: [
-            [
-              "module-resolver",
-              {
-                "alias": {
-                  "react-native": "./node_modules/react-native-web",
-                  "react-native-fs": "./.storybook/fs",
-                 // Brought in from tsconfig "paths"
-                  "@components/*": "./src/components/*",
-                  "@constants": "./src/constants",
-                  "@constants/*": "./src/constants/*",
-                  "@entities": "./src/entities",
-                  "@entities/*": "./src/entities/*",
-                  "@hooks": "./src/hooks",
-                  "@hooks/*": "./src/hooks/*",
-                  "@types": "./src/types",
-                  "@types/*": "./src/types/*",
-                  "@utils": "./src/utils",
-                  "@utils/*": "./src/utils/*",
-                  "@store": "./src/store",
-                  "@store/*": "./src/store/*",
-                  "@assets/*": "./assets/*",
-                  "@services": "./src/services",
-                  "@services/*": "./src/services/*"
-                }
-              }
-            ]
-          ],
-          cacheDirectory: true,
-        },
-      },
-  );
+module.exports = ({config, mode}) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    exclude: [/node_modules/],
+    loader: require.resolve('babel-loader'),
+    options: {
+      babelrc: false,
+      presets: [['react-app', {flow: false, typescript: true}]],
+      plugins: [
+        [
+          'module-resolver',
+          {
+            alias: {
+              'react-native': './node_modules/react-native-web',
+              'react-native-fs': './.storybook/fs',
+              // Brought in from tsconfig "paths"
+              '@components/*': './src/components/*',
+              '@constants': './src/constants',
+              '@constants/*': './src/constants/*',
+              '@entities': './src/entities',
+              '@entities/*': './src/entities/*',
+              '@hooks': './src/hooks',
+              '@hooks/*': './src/hooks/*',
+              '@types': './src/types',
+              '@types/*': './src/types/*',
+              '@utils': './src/utils',
+              '@utils/*': './src/utils/*',
+              '@store': './src/store',
+              '@store/*': './src/store/*',
+              '@assets/*': './assets/*',
+              '@services': './src/services',
+              '@services/*': './src/services/*',
+            },
+          },
+        ],
+      ],
+      cacheDirectory: true,
+    },
+  });
 
-      console.log(resolveApp('../node_modules/react-native-vector-icons'));
+  console.log(resolveApp('../node_modules/react-native-vector-icons'));
 
   config.module.rules.push({
     test: /\.(js|jsx|mjs)$/,
@@ -56,9 +54,7 @@ module.exports = ({ config, mode }) => {
     loader: require.resolve('babel-loader'),
     options: {
       babelrc: false,
-      presets: [
-        'react-app',
-      ],
+      presets: ['react-app'],
       plugins: [
         '@babel/plugin-proposal-class-properties',
         '@babel/plugin-proposal-object-rest-spread',
@@ -75,17 +71,38 @@ module.exports = ({ config, mode }) => {
     },
   });
 
+  config.module.rules.push({
+    test: /\.(gif|jpe?g|png|svg)$/,
+    use: {
+      loader: 'url-loader',
+      options: {name: '[name].[ext]'},
+    },
+  });
 
-  config.module.rules.push(
-
+  config.module.rules.push({
+    test: /\.css$/,
+    use: [
       {
-        test: /\.(gif|jpe?g|png|svg)$/,
-        use: {
-          loader: "url-loader",
-          options: { name: "[name].[ext]" }
-        }
-      }
-  );
+        loader: 'file-loader',
+        options: {
+          name: 'css/[name].[ext]',
+        },
+      },
+      'css-loader',
+    ],
+    include: [/fonts/],
+  });
+
+  config.module.rules.push({
+    test: /\.ttf$/,
+    loader: 'file-loader',
+    options: {
+      name: '[name].[ext]',
+      outputPath: 'css/',
+      publicPath: url => '../css/' + url,
+    },
+    include: path.resolve(__dirname, 'node_modules/react-native-vector-icons'),
+  });
 
   config.resolve.extensions.push('.ts', '.tsx', '.js');
 
---
+++ src/components/shark-text-input/shark-text-input.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import {SharkTextInput} from './shark-text-input';
+
+const SharkInputDemo = ({...props}: any) => {
+  const [value, setValue] = React.useState('');
+  return (
+    <SharkTextInput
+      placeholder="Type in me"
+      prefixIcon="link"
+      value={value}
+      onChangeText={setValue}
+      {...props}
+    />
+  );
+};
+
+storiesOf('Shark Input', module).add('to Storybook', () => <SharkInputDemo />);
