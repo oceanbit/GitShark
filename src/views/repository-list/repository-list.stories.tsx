a8c84a907545df9f5dbfcb74196ce48ed2a3d151
Corbin Crutchley <crutchcorn@gmail.com>
2020-05-30 16:21:37 -0700

    Initial work on getting repo list full-screen preview

--- src/views/repository-list/repository-list.stories.tsx
+++ src/views/repository-list/repository-list.stories.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {RepositoryListUI} from './repository-list.ui';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {lightPaperTheme} from '@constants';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {View} from 'react-native';
 
 const RepositoryListDemo = ({...props}: any) => {
-  return (
+  const Stack = createStackNavigator();
+
+  const Comp = () => (
     <RepositoryListUI
       isLoading={false}
       isDBLoaded={false}
@@ -11,6 +19,20 @@ const RepositoryListDemo = ({...props}: any) => {
       repos={[]}
       findRepos={() => Promise.resolve(false)}
     />
+  );
+
+  return (
+    <View style={{height: '100%'}}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <PaperProvider theme={lightPaperTheme}>
+            <Stack.Navigator headerMode={'none'}>
+              <Stack.Screen name="Test" component={Comp} />
+            </Stack.Navigator>
+          </PaperProvider>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </View>
   );
 };
 
