18bc6cc1a4636189150d3e48e84472848a59fbef
Corbin Crutchley <crutchcorn@gmail.com>
2020-05-30 14:47:38 -0700

    Added radio story

---
+++ src/components/shark-radio/shark-radio.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View} from 'react-native';
+import {storiesOf} from '@storybook/react';
+import {SharkRadio} from './shark-radio';
+
+const SharkRadioDemo = () => {
+  const [index, setIndex] = React.useState(0);
+  return (
+    <View style={{display: 'flex', flexDirection: 'row'}}>
+      <View style={{marginRight: 8}}>
+        <SharkRadio checked={index === 0} onValueChange={() => setIndex(0)} />
+      </View>
+      <View style={{marginRight: 8}}>
+        <SharkRadio checked={index === 1} onValueChange={() => setIndex(1)} />
+      </View>
+      <View style={{marginRight: 8}}>
+        <SharkRadio checked={index === 2} onValueChange={() => setIndex(2)} />
+      </View>
+    </View>
+  );
+};
+
+storiesOf('Shark Radios', module).add('to Storybook', () => <SharkRadioDemo />);
