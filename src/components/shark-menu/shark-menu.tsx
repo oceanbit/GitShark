131dbd855eab9ed4abd9e0a9664169091fe03c19
Corbin Crutchley <crutchcorn@gmail.com>
2020-06-14 20:04:00 -0700

    Make menu respect top inset
    Closes #12

--- src/components/shark-menu/shark-menu.tsx
+++ src/components/shark-menu/shark-menu.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {Menu} from 'react-native-paper';
 import {DynamicStyleSheet, useDynamicStyleSheet} from 'react-native-dark-mode';
 import {theme} from '@constants';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export const SharkMenu = ({
   children,
@@ -9,9 +10,12 @@ export const SharkMenu = ({
   ...props
 }: React.ComponentProps<typeof Menu>) => {
   const styles = useDynamicStyleSheet(dynamicStyles);
+  const insets = useSafeAreaInsets();
 
   return (
-    <Menu {...props} contentStyle={[styles.menu, contentStyle]}>
+    <Menu
+      {...props}
+      contentStyle={[styles.menu, contentStyle, {marginTop: insets.top}]}>
       {children}
     </Menu>
   );
