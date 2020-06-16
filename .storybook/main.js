2c4ba3b7ecddac57c058f79998bdddf418c2d8cc
Corbin Crutchley <crutchcorn@gmail.com>
2020-06-16 12:13:26 -0700

    Added viewport addon to storybooks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,9 @@
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
     '@storybook/addon-knobs/register',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-actions',
+    '@storybook/addon-links'
   ],
 };
