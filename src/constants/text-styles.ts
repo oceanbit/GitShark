802cd3c88fc8a470aff6e2cbf57e0144f944a3b7
Corbin Crutchley <corbin.crutchley@websystem3.com>
2020-06-23 21:20:56 -0700

    Make text style loading conditional on iOS and Android

--- src/constants/text-styles.ts
+++ src/constants/text-styles.ts
@@ -1,69 +1,73 @@
+import {Platform} from 'react-native';
+
+const iOS = Platform.OS === 'ios';
+
 export const textStyles = {
   display: {
-    fontFamily: 'rubik_bold',
-    fontWeight: 'normal' as const,
+    fontFamily: iOS ? 'Rubik' : 'rubik_bold',
+    fontWeight: iOS ? 'bold' : ('normal' as const),
     fontSize: 48,
     lineHeight: 64,
   },
 
   headline_01: {
-    fontFamily: 'rubik_bold',
-    fontWeight: 'normal' as const,
+    fontFamily: iOS ? 'Rubik' : 'rubik_bold',
+    fontWeight: iOS ? 'bold' : ('normal' as const),
     fontSize: 34,
     lineHeight: 48,
   },
 
   headline_02: {
-    fontFamily: 'rubik_bold',
-    fontWeight: 'normal' as const,
+    fontFamily: iOS ? 'Rubik' : 'rubik_bold',
+    fontWeight: iOS ? 'bold' : ('normal' as const),
     fontSize: 28,
     lineHeight: 36,
   },
 
   headline_03: {
-    fontFamily: 'rubik_medium',
-    fontWeight: 'normal' as const,
+    fontFamily: iOS ? 'Rubik' : 'rubik_medium',
+    fontWeight: iOS ? '500' : ('normal' as const),
     fontSize: 20,
     lineHeight: 28,
   },
 
   callout: {
-    fontFamily: 'rubik_medium',
-    fontWeight: 'normal' as const,
+    fontFamily: iOS ? 'Rubik' : 'rubik_medium',
+    fontWeight: iOS ? '500' : ('normal' as const),
     fontSize: 16,
     lineHeight: 24,
   },
 
   body_01: {
-    fontFamily: 'rubik_regular',
+    fontFamily: iOS ? 'Rubik' : 'rubik_regular',
     fontWeight: 'normal' as const,
     fontSize: 16,
     lineHeight: 24,
   },
 
   body_02: {
-    fontFamily: 'rubik_regular',
+    fontFamily: iOS ? 'Rubik' : 'rubik_regular',
     fontWeight: 'normal' as const,
     fontSize: 14,
     lineHeight: 20,
   },
 
   caption_01: {
-    fontFamily: 'rubik_medium',
-    fontWeight: 'normal' as const,
+    fontFamily: iOS ? 'Rubik' : 'rubik_medium',
+    fontWeight: iOS ? '500' : ('normal' as const),
     fontSize: 12,
     lineHeight: 16,
   },
 
   caption_02: {
-    fontFamily: 'rubik_regular',
+    fontFamily: iOS ? 'Rubik' : 'rubik_regular',
     fontWeight: 'normal' as const,
     fontSize: 12,
     lineHeight: 16,
   },
 
   overline: {
-    fontFamily: 'rubik_regular',
+    fontFamily: iOS ? 'Rubik' : 'rubik_regular',
     fontWeight: 'normal' as const,
     fontSize: 9,
     lineHeight: 12,
