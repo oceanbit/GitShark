e639fcef10221cc7d0bd7cc3a1fd5fb1b2994efa
Corbin Crutchley <crutchcorn@gmail.com>
2020-07-08 21:26:18 -0700

    Initial work on getting seaside 0.1 integrated in GitShark

--- src/constants/text-styles.ts
+++ src/constants/text-styles.ts
@@ -1,3 +1,4 @@
+import {getTextStyles} from 'seaside/text_styles';
 import {Platform} from 'react-native';
 
 const iOS = Platform.OS === 'ios';
@@ -22,64 +23,9 @@ export const rubikBold = {
   fontWeight: iOS ? ('bold' as const) : ('normal' as const),
 };
 
-export const textStyles = {
-  display: {
-    ...rubikBold,
-    fontSize: 48,
-    lineHeight: 64,
-  },
-
-  headline_01: {
-    ...rubikBold,
-    fontSize: 34,
-    lineHeight: 48,
-  },
-
-  headline_02: {
-    ...rubikBold,
-    fontSize: 28,
-    lineHeight: 36,
-  },
-
-  headline_03: {
-    ...rubikMedium,
-    fontSize: 20,
-    lineHeight: 28,
-  },
-
-  callout: {
-    ...rubikMedium,
-    fontSize: 16,
-    lineHeight: 24,
-  },
-
-  body_01: {
-    ...rubikRegular,
-    fontSize: 16,
-    lineHeight: 24,
-  },
-
-  body_02: {
-    ...rubikRegular,
-    fontSize: 14,
-    lineHeight: 20,
-  },
-
-  caption_01: {
-    ...rubikMedium,
-    fontSize: 12,
-    lineHeight: 16,
-  },
-
-  caption_02: {
-    ...rubikRegular,
-    fontSize: 12,
-    lineHeight: 16,
-  },
-
-  overline: {
-    ...rubikRegular,
-    fontSize: 9,
-    lineHeight: 12,
-  },
-};
+export const textStyles = getTextStyles({
+  rubikBold,
+  rubikLight,
+  rubikMedium,
+  rubikRegular,
+});
--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -3,10 +3,14 @@ import {
   DefaultTheme,
   Theme as PaperTheme,
 } from 'react-native-paper';
-import {theme as seasideTheme} from 'seaside/theme';
+import {
+  lightTheme as lightSeaside,
+  darkTheme as darkSeaside,
+  fullTheme,
+} from 'seaside/theme';
 import {Theme as NavTheme} from '@react-navigation/native/src/types';
 import {DynamicValue} from 'react-native-dark-mode';
-import {rubikLight, rubikMedium, rubikRegular} from './text-styles';
+import {rubikLight, rubikMedium, rubikRegular, textStyles} from './text-styles';
 import {opacity} from './opacity';
 
 // Taken from: https://github.com/regexhq/hsla-regex
@@ -32,70 +36,66 @@ const fontConfig: Parameters<typeof configureFonts>[0] = {
   },
 };
 
-const baseTheme = {
-  ...DefaultTheme,
-  roundness: 8,
-  disabledOpacity: 0.4,
-  lessRoundness: 4,
-  fonts: configureFonts(fontConfig),
-};
-
 export const darkPaperTheme: PaperTheme = {
-  ...baseTheme,
+  ...DefaultTheme,
   dark: true,
+  roundness: fullTheme.borderRadius.regular,
+  fonts: configureFonts(fontConfig),
   colors: {
     ...DefaultTheme.colors,
-    accent: seasideTheme.primary_dark,
-    background: seasideTheme.surface_dark,
-    error: seasideTheme.error_dark,
-    surface: seasideTheme.surface_dark,
-    text: seasideTheme.on_surface_dark,
+    accent: fullTheme.colors.primary_dark,
+    background: fullTheme.colors.surface_dark,
+    error: fullTheme.colors.error_dark,
+    surface: fullTheme.colors.surface_dark,
+    text: fullTheme.colors.on_surface_dark,
   },
 };
 
 export const lightPaperTheme: PaperTheme = {
-  ...baseTheme,
+  ...DefaultTheme,
   dark: false,
+  roundness: fullTheme.borderRadius.regular,
+  fonts: configureFonts(fontConfig),
   colors: {
     ...DefaultTheme.colors,
-    accent: seasideTheme.primary_light,
-    background: seasideTheme.surface_light,
-    error: seasideTheme.error_light,
-    surface: seasideTheme.surface_light,
-    text: seasideTheme.on_surface_light,
+    accent: fullTheme.colors.primary_light,
+    background: fullTheme.colors.surface_light,
+    error: fullTheme.colors.error_light,
+    surface: fullTheme.colors.surface_light,
+    text: fullTheme.colors.on_surface_light,
   },
 };
 
 export const darkNavTheme: NavTheme = {
   dark: false,
   colors: {
-    primary: seasideTheme.primary_dark,
-    card: seasideTheme.surface_dark,
-    background: seasideTheme.surface_dark,
-    text: seasideTheme.on_surface_dark,
-    border: seasideTheme.tint_on_surface_16_dark,
+    primary: fullTheme.colors.primary_dark,
+    card: fullTheme.colors.surface_dark,
+    background: fullTheme.colors.surface_dark,
+    text: fullTheme.colors.on_surface_dark,
+    border: fullTheme.colors.tint_on_surface_16_dark,
   },
 };
 
 export const lightNavTheme: NavTheme = {
   dark: false,
   colors: {
-    primary: seasideTheme.primary_light,
-    card: seasideTheme.surface_light,
-    background: seasideTheme.surface_light,
-    text: seasideTheme.on_surface_light,
-    border: seasideTheme.tint_on_surface_16_light,
+    primary: fullTheme.colors.primary_light,
+    card: fullTheme.colors.surface_light,
+    background: fullTheme.colors.surface_light,
+    text: fullTheme.colors.on_surface_light,
+    border: fullTheme.colors.tint_on_surface_16_light,
   },
 };
 
 /**
  * This is an awful lot of hackiness. Crazy how it still works, iddnit?
  */
-const colors = Object.keys(lightTheme).reduce(
+const colors = Object.keys(lightSeaside.colors).reduce(
   (prev, key: string) => {
     const theKey = key as 'primary';
-    const lightVal = lightTheme[theKey];
-    const darkVal = darkTheme[theKey];
+    const lightVal = lightSeaside.colors[theKey];
+    const darkVal = darkSeaside.colors[theKey];
     if (lightVal === darkVal) {
       prev[theKey] = lightVal;
     } else {
@@ -105,14 +105,18 @@ const colors = Object.keys(lightTheme).reduce(
   },
   {
     on_surface_secondary: new DynamicValue(
-      getSecondaryStatic(seasideTheme.on_surface_light),
-      getSecondaryStatic(seasideTheme.on_surface_dark),
+      getSecondaryStatic(fullTheme.colors.on_surface_light),
+      getSecondaryStatic(fullTheme.colors.on_surface_dark),
     ),
     on_surface_secondary_no_opacity: new DynamicValue('#717f9b', '#8f97a8'),
-  } as typeof lightTheme,
+  } as typeof lightSeaside.colors & {
+    on_surface_secondary: DynamicValue<string>;
+    on_surface_secondary_no_opacity: DynamicValue<string>;
+  },
 );
 
 export const theme = {
-  ...baseTheme,
+  ...fullTheme,
   colors,
+  textStyles: textStyles,
 };
