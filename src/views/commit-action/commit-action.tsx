3c34c454b711e4057157b6a00c3e7036af07cd32
Corbin Crutchley <crutchcorn@gmail.com>
2020-07-16 16:25:48 -0700

    Migrate commit action code out of UI area, etc

--- src/views/commit-action/commit-action.tsx
+++ src/views/commit-action/commit-action.tsx
@@ -1,108 +1,39 @@
 import * as React from 'react';
-import {KeyboardAvoidingView, ScrollView, Text, View} from 'react-native';
-import {theme} from '@constants';
-import {useNavigation, useRoute} from '@react-navigation/native';
-import {SharkIconButton} from '@components/shark-icon-button';
-import {FileChangeListItem} from '@components/file-change-list-item';
-import {SharkTextInput} from '@components/shark-text-input';
-import {SharkButton} from '@components/shark-button';
-import {ChangesArrayItem, commit} from '@services';
-import {DynamicStyleSheet, useDynamicStyleSheet} from 'react-native-dark-mode';
-import {SharkDivider} from '@components/shark-divider';
-import {BottomSpacerView} from '@components/shark-safe-top';
+import {useNavigation} from '@react-navigation/native';
+import {commit} from '@services';
 import {useSelector} from 'react-redux';
-import {RootState} from '@store';
+import {RootState, getGitStatus} from '@store';
+import {CommitActionUI} from './commit-action.ui';
+import {useThunkDispatch} from '@hooks';
 
 export const CommitAction = () => {
   const {repo} = useSelector((state: RootState) => state.repository);
-  const styles = useDynamicStyleSheet(dynamicStyles);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const route = useRoute<any>();
-  const getUpdate = route!.params!.updateFiles as () => any;
-  const files = route!.params!.files as ChangesArrayItem[];
+  const {staged} = useSelector((state: RootState) => state.changes);
+  const dispatch = useThunkDispatch();
+
   const history = useNavigation();
 
-  const [commitTitle, setCommitTitle] = React.useState('');
-  const [commitBody, setCommitBody] = React.useState('');
+  const getUpdate = () => {
+    dispatch(getGitStatus());
+  };
 
-  if (!getUpdate) {
-    history.navigate('Repository');
-    return null;
-  }
-
-  const onSubmit = async () => {
+  const onSubmit = async ({
+    commitBody,
+    commitTitle,
+  }: {
+    commitTitle: string;
+    commitBody: string;
+  }) => {
     await commit({repo: repo!, description: commitBody, title: commitTitle});
     getUpdate();
     history.navigate('Repository');
   };
 
   return (
-    <KeyboardAvoidingView
-      style={{flex: 1, flexDirection: 'column'}}
-      behavior="padding"
-      enabled>
-      <View style={[styles.commitHeaderContainer]}>
-        <SharkIconButton onPress={() => history.goBack()} iconName="close" />
-        <Text style={styles.commitHeader}>Commit changes</Text>
-      </View>
-      <SharkDivider />
-      <ScrollView>
-        {files.map(file => (
-          <React.Fragment key={file.fileName}>
-            <FileChangeListItem
-              fileName={file.fileName}
-              fileStatus={file.fileStatus}
-            />
-            <SharkDivider />
-          </React.Fragment>
-        ))}
-      </ScrollView>
-      <SharkDivider />
-      <View style={styles.commitData}>
-        <SharkTextInput
-          placeholder={'Commit title'}
-          value={commitTitle}
-          onChangeText={setCommitTitle}
-        />
-        <SharkTextInput
-          placeholder={'Commit description'}
-          value={commitBody}
-          onChangeText={setCommitBody}
-          numberOfLines={4}
-          style={styles.textarea}
-        />
-        <View>
-          <SharkButton
-            onPress={onSubmit}
-            text={'Commit change'}
-            type={'primary'}
-          />
-        </View>
-      </View>
-      <BottomSpacerView />
-    </KeyboardAvoidingView>
+    <CommitActionUI
+      onSubmit={onSubmit}
+      files={staged}
+      onClose={() => history.goBack()}
+    />
   );
 };
-
-const dynamicStyles = new DynamicStyleSheet({
-  commitHeaderContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'nowrap',
-    alignItems: 'center',
-    paddingVertical: theme.spacing.m,
-    paddingHorizontal: theme.spacing.xs,
-  },
-  commitHeader: {
-    marginLeft: theme.spacing.xs,
-    ...theme.textStyles.headline_03,
-    color: theme.colors.on_surface,
-  },
-  commitData: {
-    padding: theme.spacing.m,
-  },
-  textarea: {
-    height: 128,
-    marginVertical: theme.spacing.m,
-  },
-});
---
+++ src/views/commit-action/commit-action.ui.tsx
@@ -0,0 +1,98 @@
+import * as React from 'react';
+import {KeyboardAvoidingView, ScrollView, Text, View} from 'react-native';
+import {theme} from '@constants';
+import {SharkIconButton} from '@components/shark-icon-button';
+import {FileChangeListItem} from '@components/file-change-list-item';
+import {SharkTextInput} from '@components/shark-text-input';
+import {SharkButton} from '@components/shark-button';
+import {ChangesArrayItem} from '@services';
+import {DynamicStyleSheet, useDynamicStyleSheet} from 'react-native-dark-mode';
+import {SharkDivider} from '@components/shark-divider';
+import {BottomSpacerView} from '@components/shark-safe-top';
+
+interface CommitActionUIProps {
+  onSubmit: (props: {commitTitle: string; commitBody: string}) => Promise<void>;
+  files: ChangesArrayItem[];
+  onClose: () => void;
+}
+
+export const CommitActionUI = ({
+  onSubmit,
+  files,
+  onClose,
+}: CommitActionUIProps) => {
+  const styles = useDynamicStyleSheet(dynamicStyles);
+
+  const [commitTitle, setCommitTitle] = React.useState('');
+  const [commitBody, setCommitBody] = React.useState('');
+
+  return (
+    <KeyboardAvoidingView
+      style={{flex: 1, flexDirection: 'column'}}
+      behavior="padding"
+      enabled>
+      <View style={[styles.commitHeaderContainer]}>
+        <SharkIconButton onPress={onClose} iconName="close" />
+        <Text style={styles.commitHeader}>Commit changes</Text>
+      </View>
+      <SharkDivider />
+      <ScrollView>
+        {files.map(file => (
+          <React.Fragment key={file.fileName}>
+            <FileChangeListItem
+              fileName={file.fileName}
+              fileStatus={file.fileStatus}
+            />
+            <SharkDivider />
+          </React.Fragment>
+        ))}
+      </ScrollView>
+      <SharkDivider />
+      <View style={styles.commitData}>
+        <SharkTextInput
+          placeholder={'Commit title'}
+          value={commitTitle}
+          onChangeText={setCommitTitle}
+        />
+        <SharkTextInput
+          placeholder={'Commit description'}
+          value={commitBody}
+          onChangeText={setCommitBody}
+          numberOfLines={4}
+          style={styles.textarea}
+        />
+        <View>
+          <SharkButton
+            onPress={() => onSubmit({commitBody, commitTitle})}
+            text={'Commit change'}
+            type={'primary'}
+          />
+        </View>
+      </View>
+      <BottomSpacerView />
+    </KeyboardAvoidingView>
+  );
+};
+
+const dynamicStyles = new DynamicStyleSheet({
+  commitHeaderContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'center',
+    paddingVertical: theme.spacing.m,
+    paddingHorizontal: theme.spacing.xs,
+  },
+  commitHeader: {
+    marginLeft: theme.spacing.xs,
+    ...theme.textStyles.headline_03,
+    color: theme.colors.on_surface,
+  },
+  commitData: {
+    padding: theme.spacing.m,
+  },
+  textarea: {
+    height: 128,
+    marginVertical: theme.spacing.m,
+  },
+});
