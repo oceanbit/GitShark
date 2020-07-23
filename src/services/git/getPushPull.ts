fdc1a0d55a608017a803d16b00c440bac32e5096
Corbin Crutchley <crutchcorn@gmail.com>
2020-07-23 16:08:23 -0700

    Fix tracked branch assumptions with push/pull

--- src/services/git/getPushPull.ts
+++ src/services/git/getPushPull.ts
@@ -7,25 +7,16 @@ interface GetPushPullProps {
 }
 
 export const getPushPull = async ({path}: GetPushPullProps) => {
-  const currBranch: string = (await git.currentBranch({
+  const currBranch = (await git.currentBranch({
     fs,
     dir: path,
-  })) as any;
-  /**
-   * isomorphic-git does not currently have support for remote tracking
-   * (or at least, does not store the tracked branch properly)
-   *
-   * We will be working on introducing this functionality upsteam
-   * but until such time, we will move forward in this service by assuming
-   * the remote branch has the same name as the local branch (DANGEROUS assumption)
-   *
-   * Another dangerous assumption we have to make: It will track
-   */
+  })) as string;
 
-  const remotes = await git.listRemotes({fs, dir: path});
-
-  const trackedRemote = remotes[0].remote;
-  const trackedBranch = `${trackedRemote}/${currBranch}`;
+  const trackedBranch = await git.getRemoteTrackingBranch({
+    fs,
+    dir: path,
+    ref: currBranch,
+  });
 
   const {branch1Diff: toPull, branch2Diff: toPush} = await revList({
     dir: path,
