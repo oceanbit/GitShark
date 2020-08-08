d7a83d172e2ff8e6a5543ca4be8e54f3047cb127
Corbin Crutchley <crutchcorn@gmail.com>
2020-08-08 15:28:00 -0700

    chore: added back old clone repo method for iOS

--- src/services/git/cloneRepo.ts
+++ src/services/git/cloneRepo.ts
@@ -1,7 +1,9 @@
-import {ProgressCallback} from 'isomorphic-git/index.umd.min.js';
+import git, {ProgressCallback} from 'isomorphic-git/index.umd.min.js';
+import {fs} from '@constants';
+import http from 'isomorphic-git/http/web/index.js';
 import {createNewRepo} from './createRepo';
 import {getRepoNameFromUri} from '@utils';
-import {NativeEventEmitter, NativeModules} from 'react-native';
+import {NativeEventEmitter, NativeModules, Platform} from 'react-native';
 
 interface CloneRepoProps {
   path: string;
@@ -11,27 +13,59 @@ interface CloneRepoProps {
 }
 
 export const cloneRepo = ({path, name, uri, onProgress}: CloneRepoProps) => {
-  const eventEmitter = new NativeEventEmitter(NativeModules.GitModule);
+  const newFolderName = getRepoNameFromUri(uri);
+  const repoName = name || newFolderName;
+  const repoDir = `${path}/${repoName}`;
 
-  const eventListener = eventEmitter.addListener(
-    'CloneProgress',
-    (event: {phase: string; loaded: number; total: number}) => {
-      const {phase, loaded, total} = event;
-      onProgress({phase, loaded, total});
-    },
-  );
+  if (Platform.OS === 'android') {
+    const eventEmitter = new NativeEventEmitter(NativeModules.GitModule);
 
-  return new Promise((resolve, reject) => {
-    const newFolderName = getRepoNameFromUri(uri);
-    const repoName = name || newFolderName;
-    const repoDir = `${path}/${repoName}`;
+    const eventListener = eventEmitter.addListener(
+      'CloneProgress',
+      (event: {phase: string; loaded: number; total: number}) => {
+        const {phase, loaded, total} = event;
+        onProgress({phase, loaded, total});
+      },
+    );
 
-    NativeModules.GitModule.clone(uri, repoDir)
-      .then(() => {
-        eventListener.remove();
-      })
-      .then(() => createNewRepo(repoDir, repoName))
-      .then(() => resolve())
-      .catch((e: Error) => reject(e));
-  });
+    return new Promise((resolve, reject) => {
+      NativeModules.GitModule.clone(uri, repoDir)
+        .then(() => {
+          eventListener.remove();
+        })
+        .then(() => createNewRepo(repoDir, repoName))
+        .then(() => resolve())
+        .catch((e: Error) => reject(e));
+    });
+  }
+
+  /**
+   * Please be aware that this code will likely cause `OutOfMemory` errors on mobile platforms
+   * As such, this code should be replaced with native `git` wrapper code. This is only here as a temporary safegaurd
+   */
+  return git
+    .clone({
+      fs,
+      dir: repoDir,
+      url: uri,
+      http,
+      singleBranch: true,
+      onProgress,
+    })
+    .then(() =>
+      /**
+       * Isomorphic git doesn't fetch as we might expect it to after a clone
+       * @see https://github.com/crutchcorn/GitShark/issues/33
+       */
+      git.fetch({
+        fs,
+        http,
+        dir: repoDir,
+        url: uri,
+        singleBranch: false,
+        prune: false,
+        onProgress,
+      }),
+    )
+    .then(() => createNewRepo(repoDir, repoName));
 };
