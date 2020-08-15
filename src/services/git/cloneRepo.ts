af56c93f719aecf3da3a9eb00c89e233eb05f1ee
Corbin Crutchley <crutchcorn@gmail.com>
2020-08-14 18:41:24 -0700

    chore(build): publish version code changes

--- src/services/git/cloneRepo.ts
+++ src/services/git/cloneRepo.ts
@@ -55,7 +55,7 @@ export const cloneRepo = ({path, name, uri, onProgress}: CloneRepoProps) => {
     .then(() =>
       /**
        * Isomorphic git doesn't fetch as we might expect it to after a clone
-       * @see https://github.com/crutchcorn/GitShark/issues/33
+       * @see https://github.com/oceanbit-dev/GitShark/issues/33
        */
       git.fetch({
         fs,
