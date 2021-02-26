aede08d6d91f268115d20159266858895c8b47bd
Corbin Crutchley <crutchcorn@gmail.com>
2021-02-25 18:36:12 -0800

    chore: im the map

---
+++ src/components/commit-pill/im-the-map.tsx
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+
+const ImTheMap = () => (
+  <View>
+    <Text>
+      If there’s a place you got to go I’m the one you need to know I’m the map
+      I’m the map, I’m the map If there’s a place you got to get I can get you
+      there, I bet I’m the map I’m the map, I’m the map I’m the map, I’m the map
+      I’m the map, I’m the map I’m the map, I’m the map I’m the map, I’m the map
+      I’m the map
+    </Text>
+  </View>
+);
