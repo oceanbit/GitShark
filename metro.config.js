d5e0e2e7660b08a31d94cd96561f6a9f3c571697
Corbin Crutchley <crutchcorn@gmail.com>
2021-03-12 16:09:50 -0800

    chore: upgrade to RN 0.64

--- metro.config.js
+++ metro.config.js
@@ -4,31 +4,14 @@
  *
  * @format
  */
-const path = require('path');
-const extraNodeModules = {
-  githubIssues: path.resolve(__dirname + '/.github/ISSUE_TEMPLATE'),
-};
-const watchFolders = [path.resolve(__dirname + '/.github/ISSUE_TEMPLATE')];
-
-// get defaults assetExts array
-const defaultAssetExts = require('metro-config/src/defaults/defaults')
-  .assetExts;
 
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
-  resolver: {
-    extraNodeModules,
-    assetExts: [
-      ...defaultAssetExts, // <- array spreading defaults
-      'md',
-    ],
-  },
-  watchFolders,
 };
