8ab00a05bffbe37d27fb4a8a1618d1b4ac4d56f1
Corbin Crutchley <crutchcorn@gmail.com>
2021-05-08 01:59:28 -0700

    chore: replace responsive ui with npm build

--- src/types/responsive-ui.d.ts
+++
@@ -1,4 +0,0 @@
-// Remove when https://github.com/wcandillon/react-native-responsive-ui/pull/71 is merged
-module 'react-native-responsive-ui' {
-  export * from 'react-native-responsive-ui/dist/typescript/index';
-}
